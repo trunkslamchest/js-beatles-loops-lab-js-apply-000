1c76d68dceb4ca7d7f80fd00951432c9e4957253
Austin Smith <trunk9slamchest@gmail.com>
2019-06-25 16:48:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function johnLennonFacts(array) {
 function iLoveTheBeatles(n) {
   var new_arr = []
   do {
-  i = i + 1;
-  new_arr.push('I love the Bearles!');
+    i = i + 1;
+    new_arr.push('I love the Bearles!');
   } while (i < 15 - num);
 }
