2827b97f45771ada5f2eeca36bfc4059864c6f4b
Austin Smith <trunk9slamchest@gmail.com>
2019-06-25 16:45:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var new_arr = []
-    for (i = 0; i < 4 ; i++) {
+    for (var i = 0; i < 4 ; i++) {
       new_arr.push(musicians.shift() + ' plays ' + instruments.shift())
     }
   return new_arr
